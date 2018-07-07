396d10996d4762b6f6650f01658db7c226f82c92
SYDDHANTT JAIN <siddhantn7@gmail.com>
2018-07-07 16:19:29 -0700

    Final commit

---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,23 @@
+import addNewContact from '../controllers/crmController';
+import {getAllContacts,getContactById,updateContact,deleteContact}from '../controllers/crmController';
+
+const routes = (app) =>{
+    app.route('/contact')
+    .get((req,res,next)=>{
+        //middleware
+        console.log(`Request from: ${req.originalUrl}`)
+        console.log(`Request from: ${req.method}`)
+        next();
+    }, getAllContacts)
+        
+    .post(addNewContact);
+
+    app.route('/contact/:contactId')
+
+    .get(getContactById)
+    .put(updateContact)
+    .delete(deleteContact);
+    
+}
+
+export default routes;
